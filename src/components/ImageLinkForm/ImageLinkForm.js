e0a96f35e3d5e9fd43b3866200eb7045c47ae7b9
Srishti Suman <76532197+srishti3418@users.noreply.github.com>
2021-07-04 14:07:53 +0530

    Adding placeholder.

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 		</p>
 		<div className = 'center'>
 		<div className = 'form center   pa4 br3 shadow-5'>
-			<input className = 'f4 pa2 w-70 center' type = 'tex' onChange = {onInputChange} placeholder='paste url of any image containing face'/>
+			<input className = 'f4 pa2 w-70 center' type = 'tex' onChange = {onInputChange} placeholder='paste url of any image containing face.'/>
 			<button className = 'w-30 grow f4 link ph3 pv2 dib white bg-light-purple' 
 			onClick = {onButtonSubmit}>Detect</button>
 		</div>
